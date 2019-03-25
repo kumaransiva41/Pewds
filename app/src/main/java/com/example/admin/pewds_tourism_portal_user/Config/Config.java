63146fc63c5e191ca69e863a41091cc5cef96246
SivaKumaran <kumaransiva41@gmail.com>
2019-03-26 00:00:23 +0530

    added something

---
+++ app/src/main/java/com/example/admin/pewds_tourism_portal_user/Config/Config.java
@@ -0,0 +1,8 @@
+package com.example.admin.pewds_tourism_portal_user.Config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID  ="AXMcGIoFJs0XsEw7SpY9wOF5I24rBwlq7hY7Ninph13Hf0rXt61SkCCQJR4NjDfEtPMv04janaXcWriM";
+
+
+
+}
